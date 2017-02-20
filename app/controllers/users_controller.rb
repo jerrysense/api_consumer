27d097d16f666d4c62fb4b33530f6ea963eeffd0
Jeremiah Kendall <jerrysense@gmail.com>
2017-02-19 23:19:44 -0800

    Fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,16 @@ class UsersController < ApplicationController
 
   def index
     uri = "#{API_BASE_URL}/users.json" # specifying json format in the URl
-    rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD) # It will create
-    new rest-client resource so that we can call different methods of it
-    users = rest_resource.get # will get back you all the detail in json format, but it
-    will we wraped as string, so we will parse it in the next step.
-    @users = JSON.parse(users, :symbolize_names => true) # we will convert the return
-    data into array of hash.see json data parsing here
+    rest_resource = RestClient::Resource.new(uri, USERNAME, PASSWORD) # It will create new rest-client resource so that we can call different methods of it
+    users = rest_resource.get # will get back you all the detail in json format, but it will we wraped as string, so we will parse it in the next step.
+    @users = JSON.parse(users, :symbolize_names => true) # we will convert the return data into array of hash.see json data parsing here
   end
 
   def new
 
   end
 
-  NOTE :  see below how we are passing the payload and content type while making the post call
+  #NOTE :  see below how we are passing the payload and content type while making the post call
   def create
     uri = "#{API_BASE_URL}/users"
     payload = params.to_json # converting the params to json
